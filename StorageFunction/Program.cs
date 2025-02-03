ab01636a48356980b363b8fc94a8edb9431ffe7f
acsf <a.carlossf@hotmail.com>
2025-02-03 19:44:50 -0300

    Adicionar arquivos de projeto.

---
+++ StorageFunction/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+
+builder.Services.Configure<KestrelServerOptions>(opt =>
+{
+    opt.Limits.MaxRequestBodySize = 104857600;
+});
+
+builder.Build().Run();
